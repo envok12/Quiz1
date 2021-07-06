e2850d5a3a6201faf44fbc01aeaeeca9980265f1
s12mitchell <s12mitchell@gmail.com>
2021-07-06 10:14:01 -0600

    Add project files.

---
+++ Quiz1/Force.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz1
+{
+    class Force
+    {
+        private const double g = 0.0000000000667408;
+        private double mass1 = 0.0;
+        private double mass2 = 0.0;
+        private double distance = 0.0;
+        private double gravitationalForce = 0.0;
+
+        public double MassOfObject1
+        {
+            get
+            {
+                return mass1;
+            }
+            set
+            {
+                mass1 = value;
+                Calc();
+
+            }
+        }
+
+        public double MassOfObject2
+        {
+            get
+            {
+                return mass2;
+            }
+            set
+            {
+                mass2 = value;
+                Calc();
+            }
+        }
+
+        public double DistanceBetweenObjects
+        {
+            get
+            {
+                return distance;
+            }
+            set
+            {
+                distance = value;
+                Calc();
+
+            }
+        }
+
+        public double GravitationalForce
+        {
+            
+            get
+            {
+                return gravitationalForce;
+            }
+            private set
+            {
+                gravitationalForce = value;
+            }
+            
+        }
+
+        private void Calc()
+        {
+
+            gravitationalForce = g * (mass1 * mass2) / (distance * distance);
+        }
+    }
+}
---
+++ Quiz1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string another = " ";
+            do
+            {
+                Force force = new Force();
+
+                Console.WriteLine("Please enter the mass of the first object in kilograms: ");
+                double mass1 = Convert.ToDouble(Console.ReadLine());               
+
+                Console.WriteLine("Please enter the mass of the second object in kilograms: ");
+                double mass2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please enter the distance between the two objects in meters: ");
+                double distance = Convert.ToDouble(Console.ReadLine());
+
+                
+                force.MassOfObject1 = mass1;
+                force.MassOfObject2 = mass2;
+
+                force.DistanceBetweenObjects = distance;
+
+                Console.WriteLine("The force is: {0}", force.GravitationalForce);
+
+                Console.WriteLine("Would you like to do another? Please enter yes or no.");
+                another = Console.ReadLine();
+
+            } while (another.Equals("yes"));
+        }
+    }
+}
